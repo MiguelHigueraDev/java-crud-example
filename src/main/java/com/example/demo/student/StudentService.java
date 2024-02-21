db1e5fb93c698c6fffd841a8f9294ef5a7b41e69
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-02-21 16:52:55 -0300

    feat: add put (updateStudent)

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,13 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -49,6 +51,24 @@ public class StudentService {
             return studentOptional.get();
         } else {
             throw new IllegalStateException("Student with id " + studentId + " does not exist.");
+        }
+    }
+
+    // PUT
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException(("Student with ID " + studentId + " does not exist.")));
+
+        if (name != null && !name.isEmpty() && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && !email.isEmpty() && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("Email already taken.");
+            }
+            student.setEmail(email);
         }
     }
 }
