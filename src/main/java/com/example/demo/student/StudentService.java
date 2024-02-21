92a4de52e917f9461eb09fb8ff4ed5c279ecc0ee
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-02-21 16:25:40 -0300

    feat: add create

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -17,7 +18,19 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
+    // READ
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    // CREATE
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        // Todo: Improve validations
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("Email already taken.");
+        }
+        studentRepository.save(student);
+
     }
 }
